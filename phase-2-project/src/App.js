74b7238b292c7e8ea0f9fe1efcb59eef2a63a77c
Brad Bieselin <bradley.bieselin@gmail.com>
2022-05-09 10:32:44 -0400

    Initial commit

---
+++ phase-2-project/src/App.js
@@ -0,0 +1,12 @@
+import CardContainer from './components/CardContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <header>header</header>
+      <CardContainer />
+    </div>
+  );
+}
+
+export default App;
